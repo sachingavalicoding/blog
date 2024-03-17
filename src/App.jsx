0bbff39ffb42e0b465ac5abebfedb20fdddc9781
Sachin Gavali <sachingavali321@gmail.com>
2024-03-18 05:17:54 +0530

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signin from "./pages/auth/Signin";
 import Signup from "./pages/auth/Signup";
-import { UserProvider } from "./context/UserContext";
+
 const App = () => {
-  const isLoginPage = window.location.pathname === "/login";
   return (
     <div>
-      <UserProvider>
-        <main>{isLoginPage ? <Signup /> : <Home />}</main>
-      </UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
